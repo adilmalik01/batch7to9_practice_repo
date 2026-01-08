2b53225f84f51ae005715bb8675ee10f851b0a79
adilmalik01 <malikadil5679@gmail.com>
2026-01-08 20:18:40 +0500

    update : app dart file add

---
+++ app.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void main() {
+//   try {
+//     var result = 2 / 4;
+//     print(result);
+//   } catch (e) {
+//     print("error , $e");
+//   } finally {
+//     print("program end");
+//   }
+  
+  print("Enter you Birth place");
+  
+  String BirthPlace = stdin.readLineSync();
+
+  print("your Birth place is $BirthPlace");
+  
+  
+}
