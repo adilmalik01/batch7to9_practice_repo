5cf265b1a7888fedc1a72b4b0fb66709de1af451
adilmalik01 <malikadil5679@gmail.com>
2026-01-08 20:08:58 +0500

    update: first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Counter from "./counter/counter"
+
+
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>redux</h1>
+     <Counter/>
+    </>
+  )
+}
+
+export default App
---
+++ src/counter/counter.jsx
@@ -0,0 +1,21 @@
+// import {useDispatch , useSelector} from "react-redux"''
+import { useSelector , useDispatch } from "react-redux";
+import { increment , decrement } from "./counterSlice";
+
+
+const Counter = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  
+return(<>
+ <div>
+    <h1>{count}</h1>
+    <button onClick={() => dispatch(increment())}>Increment</button>
+    <button onClick={() => dispatch(decrement())}>Decrement</button>
+
+ </div>
+</>)
+
+}
+
+export default Counter
